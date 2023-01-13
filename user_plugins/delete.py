177e8ae8c26bc702ce8ab8d74bf52c45cec018b9
Darkzz Angel <115702004+RJijin@users.noreply.github.com>
2023-01-13 15:00:01 +0530

    defined error fixed

--- user_plugins/delete.py
+++ user_plugins/delete.py
@@ -12,6 +12,8 @@ from config import Config
 
 @Client.on_message(filters.group & (filters.bot|filters.text), group = 1)
 async def delete_(client, message):
+    userid = message.from_user.id if message.from_user else None
+    group_id = message.chat.id
     try:
         st = await client.get_chat_member(group_id, userid)
         if st.status in [enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.OWNER] or message.from_user.id in Config.OWNER:
